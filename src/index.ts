b63edefc0532bcdd38dab78f132b028f5a326d88
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-01-26 19:04:09 -0300

    feat: config routes and catch http exception middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import express from 'express'
+import 'express-async-errors'
+import express, { Request, Response, NextFunction } from 'express'
 import colors from 'colors'
 
+import routes from './routes'
+
 const app = express()
+
+app.use(express.json())
+app.use(routes)
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  return response.json({
+    status: 'Error',
+    message: error.message
+  })
+})
 
 app.listen(
   3000,
