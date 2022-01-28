ce69d6bf0885d084bb383c172f28043a067bc794
Matheus Campos <matheus.rj.ig@gmail.com>
2022-01-28 00:56:21 -0300

    feat: add courses route and ensure authentication middleware

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import AuthenticateUserController from './controllers/authenticate-user-controller'
 import CreateUserController from './controllers/create-user-controller'
+import { ensureAuthenticated } from './middlewares/ensure-authenticated'
 
 const router = Router()
 
@@ -10,5 +11,31 @@ const authenticateUserController = new AuthenticateUserController()
 
 router.post('/user', createUserController.handle)
 router.post('/login', authenticateUserController.handle)
+router.get('/courses', ensureAuthenticated, (request, response) => {
+  return response.json(
+    [
+      {
+        id: 1,
+        name: 'NodeJS'
+      },
+      {
+        id: 2,
+        name: 'ReactJS'
+      },
+      {
+        id: 3,
+        name: 'React Native'
+      },
+      {
+        id: 4,
+        name: 'Flutter'
+      },
+      {
+        id: 5,
+        name: 'Elixir'
+      },
+    ]
+  )
+})
 
 export default router
