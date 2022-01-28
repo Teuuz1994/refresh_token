15448517111d5145fb3f5f9095a17aedd5f79d72
Matheus Campos <matheus.rj.ig@gmail.com>
2022-01-28 00:26:03 -0300

    feat: add authentication user use case

--- src/use-cases/authentication-user/index.ts
+++ src/use-cases/authentication-user/index.ts
@@ -1,4 +1,6 @@
 import { compare } from "bcryptjs";
+import { sign } from 'jsonwebtoken';
+
 import { client } from "../../prisma/client";
 
 interface HttpRequest {
@@ -7,7 +9,7 @@ interface HttpRequest {
 }
 
 export default class AuthenticationUser {
-  async handle({ username, password }: HttpRequest) {
+  async execute({ username, password }: HttpRequest) {
     const userAlreadyExists = await client.user.findFirst({
       where: {
         username
@@ -23,5 +25,12 @@ export default class AuthenticationUser {
     if (!passwordMatch) {
       throw new Error('User or password is incorrect, try again')
     }
+
+    const token = sign({}, 'c0718872-f15a-4deb-bc30-a445d795dadb', {
+      subject: userAlreadyExists.id,
+      expiresIn: '20s'
+    })
+
+    return { token }
   }
 }
