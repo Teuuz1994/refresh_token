25d510d538b349b845a606310f8868f2ed227152
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-01-28 17:04:01 -0300

    feat: add refresh token controller in routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,18 @@ import { Router } from 'express'
 
 import AuthenticateUserController from './controllers/authenticate-user-controller'
 import CreateUserController from './controllers/create-user-controller'
+import RefreshTokenController from './controllers/refresh-token-controller'
 import { ensureAuthenticated } from './middlewares/ensure-authenticated'
 
 const router = Router()
 
 const createUserController = new CreateUserController()
 const authenticateUserController = new AuthenticateUserController()
+const refreshTokenController = new RefreshTokenController()
 
 router.post('/user', createUserController.handle)
 router.post('/login', authenticateUserController.handle)
+router.post('/refresh-token', refreshTokenController.handle)
 router.get('/courses', ensureAuthenticated, (request, response) => {
   return response.json(
     [
